c34d5a792a230c0b5e56673f2097217eb170ae16
Daniel Puterman <daniel.puterman@applitools.com>
2019-10-04 01:07:32 +0300

    Version up + CHANGELOG

---
+++ CHANGELOG.md
@@ -0,0 +1,5 @@
+
+## [4.1.0] - 2019-07-27
+### Added 
+- This CHANGELOG file.
+- `cut()` API to Target.
--- eyes.selenium.java/src/main/java/com/applitools/eyes/selenium/Eyes.java
+++ eyes.selenium.java/src/main/java/com/applitools/eyes/selenium/Eyes.java
@@ -161,7 +161,7 @@ public class Eyes extends EyesBase {
 
     @Override
     public String getBaseAgentId() {
-        return "eyes.selenium.java/4.0.12";
+        return "eyes.selenium.java/4.1.0";
     }
 
     public EyesWebDriver getEyesDriver() {
