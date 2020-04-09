85d71b3943ddc254e0e9a3413d946cf251282c7a
Daniel Puterman <daniel.puterman@applitools.com>
2020-04-10 00:38:53 +0300

    selenium: RFE Trello 1663 (cont.) - take standard screenshot if viewportScreenshot failed

--- eyes.selenium.java/src/main/java/com/applitools/eyes/selenium/capture/MobileViewportScreenshotImageProvider.java
+++ eyes.selenium.java/src/main/java/com/applitools/eyes/selenium/capture/MobileViewportScreenshotImageProvider.java
@@ -3,8 +3,7 @@ package com.applitools.eyes.selenium.capture;
 import com.applitools.eyes.Logger;
 import com.applitools.eyes.capture.ImageProvider;
 import com.applitools.utils.ImageUtils;
-import org.openqa.selenium.JavascriptExecutor;
-import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.*;
 
 import java.awt.image.BufferedImage;
 
@@ -21,7 +20,17 @@ public class MobileViewportScreenshotImageProvider implements ImageProvider {
     @Override
     public BufferedImage getImage() {
         logger.verbose("Getting screenshot as base64...");
-        String screenshot64 = (String) ((JavascriptExecutor)driver).executeScript("mobile: viewportScreenshot");
+        String screenshot64;
+        try {
+            logger.verbose("Trying viewportScreenshot command..");
+            screenshot64 = (String) ((JavascriptExecutor) driver).executeScript("mobile: viewportScreenshot");
+            logger.verbose("Done!");
+        } catch (WebDriverException e) {
+            logger.verbose("Got the following exception for viewportScreenshot command: " + e);
+            logger.verbose("Using standard screenshot instead...");
+            screenshot64 = ((TakesScreenshot)driver).getScreenshotAs(OutputType.BASE64);
+            logger.verbose("Done!");
+        }
         logger.verbose("Done getting base64! Creating BufferedImage...");
         return ImageUtils.imageFromBase64(screenshot64);
     }
