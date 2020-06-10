4d2727186ba4b3e474a41b3e9c667a0d769fb78d
Daniel Puterman <daniel.puterman@applitools.com>
2020-06-10 13:53:53 +0300

    Version up + agentId + CHANGELOG (due to previuos publishing failure)

--- eyes.appium.java/src/main/java/com/applitools/eyes/appium/Eyes.java
+++ eyes.appium.java/src/main/java/com/applitools/eyes/appium/Eyes.java
@@ -40,7 +40,7 @@ public class Eyes extends com.applitools.eyes.selenium.Eyes {
 
     @Override
     public String getBaseAgentId() {
-        return "eyes.appium.java/4.13.0";
+        return "eyes.appium.java/4.13.1";
     }
 
     private void init() {
